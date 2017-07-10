2da7e8ede38955bfed79f4fff7f576a0eed9db42
AUTHENTICGIT <leogwork@gmail.com>
2017-07-10 21:07:33 +0800

    Urllib Request

--- mor/urllib_request_urlopen.py
+++ mor/urllib_request_urlopen.py
@@ -1,8 +1,19 @@
 import urllib.request
 import urllib.parse
+import socket
+import urllib.error
 
+'''data参数'''
 data = bytes(urllib.parse.urlencode({'word': 'hello'}), encoding='utf8')
-
 response = urllib.request.urlopen('http://httpbin.org/post', data=data)
-
 print(response.read())
+
+'''timeout参数'''
+response = urllib.request.urlopen('http://httpbin.org/get', timeout = 1)
+print(response.read())
+
+try:
+    response = urllib.request.urlopen('http://httpbin.org/get', timeout = 0.1)
+except urllib.error.URLError as e:
+    if isinstance(e.reason, socket.timeout):
+        print('TIME OUT')
